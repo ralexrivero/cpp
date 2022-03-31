1f90b6021ca19cdf122b34f538de797a45d6b061
ralexrivero <ralexrivero@gmail.com>
2022-03-31 02:24:40 -0300

    ternary operator

---
+++ 15-ternary_operator.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+using namespace std;
+
+int main(void)
+{
+    int x = 20;
+    int y = 15;
+
+    printf("x = %d, y = %d. The greater is: %d\n", x, y, x > y ? x : y);
+
+    return (0);
+}
