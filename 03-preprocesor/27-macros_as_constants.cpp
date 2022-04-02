cf1cf3debdba0b53a87c6d55d811075c88843c70
ralexrivero <ralexrivero@gmail.com>
2022-04-02 18:57:39 -0300

    preprocessor macros as constants

---
+++ 03-preprocesor/27-macros_as_constants.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+using namespace std;
+
+#define ONE 1
+#define GREET "Hello there"
+
+int main(void)
+{
+    printf("Macro ONE is: %d\n", ONE);
+    printf("Macro GREET Is: %s\n", GREET);
+
+    return (0);
+}
