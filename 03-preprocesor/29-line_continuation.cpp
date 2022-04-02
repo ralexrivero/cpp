93c12b63b54f599be710182a6e0ec80d1674abbf
ralexrivero <ralexrivero@gmail.com>
2022-04-02 19:50:51 -0300

    line continuation

---
+++ 03-preprocesor/29-line_continuation.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+using namespace std;
+
+int main(void)
+{
+    int ia[5] = {1, 2, 3, 4, 5};
+
+    printf("values:\n\
+\tia[0] = %d\n\
+\tia[1] = %d\n\
+\tia[2] = %d\n\
+\tia[3] = %d\n\
+\tia[4] = %d\n",\
+ia[0], ia[1], ia[2], ia[3], ia[4]);
+
+    return (0);
+}
